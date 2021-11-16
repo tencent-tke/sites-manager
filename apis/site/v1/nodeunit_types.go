277f6fc3131f18b978a7bd547ead137988cb9e27
attleewang <attleewang@tencent.com>
2021-11-16 11:55:28 +0800

    Init sites-manager

---
+++ apis/site/v1/nodeunit_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2021 The SuperEdge authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// NodeUnitSpec defines the desired state of NodeUnit
+type NodeUnitSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of NodeUnit. Edit nodeunit_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// NodeUnitStatus defines the observed state of NodeUnit
+type NodeUnitStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// NodeUnit is the Schema for the nodeunits API
+type NodeUnit struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   NodeUnitSpec   `json:"spec,omitempty"`
+	Status NodeUnitStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// NodeUnitList contains a list of NodeUnit
+type NodeUnitList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []NodeUnit `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&NodeUnit{}, &NodeUnitList{})
+}
